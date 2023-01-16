a05fb28c1f0e1f31f759edd8b6e3a7abd2a90604
SACHIN SINGH <200104049@hbtu.ac.in>
2023-01-16 17:38:49 +0530

    changes

--- src/helper.js
+++ src/helper.js
@@ -1,2 +1,2 @@
 // export const BASE_URL="http://localhost:5000"
-export const BASE_URL="https://netflixclonebackend-xhy5.onrender.com"
+export const BASE_URL="https://netflix-clone-backend-ashy.vercel.app"
