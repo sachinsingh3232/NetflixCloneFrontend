fd9b0691a566d38d37cdd5665fc0f9c8856b65c0
SACHIN SINGH <92503784+sachinsingh3232@users.noreply.github.com>
2023-01-17 22:40:32 +0530

    Update helper.js

--- src/helper.js
+++ src/helper.js
@@ -1,2 +1,2 @@
 // export const BASE_URL="http://localhost:5000"
-export const BASE_URL="https://netflixclone-backend-f4a901vwp-sachinsingh3232.vercel.app"
+export const BASE_URL="https://netflixclone-backend.vercel.app"
